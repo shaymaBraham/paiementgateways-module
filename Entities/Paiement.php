a5dc5fb4d68b2d9da880a730e4ec9c2f7427a6e2
shaymaBraham <shaymaabh@gmail.com>
2022-03-11 09:29:31 +0100

    bug fix user

--- Entities/Paiement.php
+++ Entities/Paiement.php
@@ -4,6 +4,7 @@ namespace Modules\PaiementGateways\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 
 class Paiement extends Model
 {
